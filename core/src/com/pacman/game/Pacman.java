a8af15a3c169268cac5dc7cfdce55d960c31b2fc
mo <tengmo@gmail.com>
2016-10-07 15:45:54 +0700

    update4

--- core/src/com/pacman/game/Pacman.java
+++ core/src/com/pacman/game/Pacman.java
@@ -10,11 +10,12 @@ public class Pacman {
     public static final int DIRECTION_STILL = 0;
     private static final int [][] DIR_OFFSETS = new int [][] {
 	        {0,0},
-	        {0,-1},
-	        {1,0},
 	        {0,1},
+	        {1,0},
+	        {0,-1},
 	        {-1,0}
 	    };
+	 public static final int SPEED = 10;
 	
 	private Vector2 position;
 	
@@ -25,8 +26,8 @@ public class Pacman {
 		return position;
 	}
 	public void move(int dir){
-		position.x += 10 * DIR_OFFSETS[dir][0];
-	    position.y += 10 * DIR_OFFSETS[dir][1];
+		position.x += SPEED * DIR_OFFSETS[dir][0];
+	    position.y += SPEED * DIR_OFFSETS[dir][1];
 	}
 
 }
